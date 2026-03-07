fade19f215cf75af2370dc66ff41e20ad31701e0
shreyasbhakta <shreyasbhakta@gmail.com>
2026-03-07 00:32:35 -0500

    minor fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // ReactPress expects output in a folder named "build"
+  build: {
+    outDir: 'build',
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
